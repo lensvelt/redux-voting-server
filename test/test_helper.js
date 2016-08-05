3f93ae08f12a257bac16941d49395ff0f1e08675
Brett <brett@applogix.com>
2016-08-05 10:14:27 -0700

    initial imuutability unit test

---
+++ test/test_helper.js
@@ -0,0 +1,5 @@
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+
+//plug in chai-immutable before any tests are run & have mocha require this file before running tests
+chai.use(chaiImmutable);
