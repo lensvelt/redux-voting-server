66a0072636bb1410cd429554c1b25025289cf7f7
Brett <brett@applogix.com>
2016-08-05 23:40:07 -0700

    Added 'next' function

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
 
-import { setEntries } from '../src/core';
+import { setEntries, next } from '../src/core';
 
 describe('application logic', () => {
 
@@ -16,6 +16,32 @@ describe('application logic', () => {
       }));
     });
 
+    it('converts to immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    })
+
+  })
+
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      })
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    })
+    
   })
 
 })
