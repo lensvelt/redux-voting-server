5eaba8ba9bb2388a821bd1af1397630497d07b9f
Brett <brett@applogix.com>
2016-08-09 19:12:37 -0700

    add initial entries to vote on and start first vote pair

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ export const store = makeStore();
 //Integrate the socket server and the redux store
 startServer(store);
 
+//Load up the starting list of entries to be voted on
 store.dispatch({
   type: 'SET_ENTRIES',
   entries: require('./entries.json')
 });
+//Kick off the first vote
 store.dispatch({ type: 'NEXT' });
